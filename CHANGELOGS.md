076a86c53cb1c6f54e455a59dbe15afc931bd304
Elias <eliasnemr@gmail.com>
2023-09-28 22:16:23 +0300

    Grid layout added

---
+++ CHANGELOGS.md
@@ -0,0 +1,2 @@
+###### 28 Sept 2023
++ grid layout added
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid grid-rows-1 grid-cols-[56px_1fr] h-full">
-      <header className="p-4">Nemr</header>
-      <main>content..</main>
+    <div className="grid grid-cols-1 grid-rows-[56px_1fr] h-screen">
+      <header className="grid grid-cols-[1fr_minmax(0,_560px)_1fr] grid-rows-1">
+        <div />
+        <div>
+          <h1>Nemr</h1>
+        </div>
+        <div />
+      </header>
+      <main className="grid grid-cols-[1fr_minmax(0,_560px)_1fr] grid-rows-1">
+        <div />
+        <section>Content..</section>
+        <div />
+      </main>
     </div>
   );
 }
