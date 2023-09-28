46524e516492b85de6f263bd8242e06611cf48d0
Elias <eliasnemr@gmail.com>
2023-09-28 21:28:31 +0300

    postcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid grid-rows-1 grid-cols-[56px_1fr] h-full p-4">
-      <header>Nemr</header>
+    <div className="grid grid-rows-1 grid-cols-[56px_1fr] h-full">
+      <header className="p-4">Nemr</header>
       <main>content..</main>
     </div>
   );
